e801284d3b8bd9bad3c567fa5fe957830bfb80fa
Jean Junior <jclgjunior@gmail.com>
2020-05-16 20:38:38 -0300

    cep-promise added && redux variables connect started

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux'
 import useStyles from './styles'
 import { Form } from '@unform/web';
 import Input from '../components/Unform/Input'
+import cep from 'cep-promise';
 import { 
   Container, Typography, Grid, Button, Box
  } from '@material-ui/core'
@@ -9,17 +11,44 @@ import {
 import Copyright from '../components/page/copyright';
 
 function Signup() {
-
+  
+  const dispach = useDispatch();
+  const [StatusAddress, setStatusAddress] = useState(false);
+  const [zipcode, setZipcode] = useState('');
+  const [address, setAddress] = useState({});
+  const endereco = useSelector(state => state.address);
+  
   const { paper, form, submit } = useStyles();
+  
+  useEffect(()=>{
+    console.log(zipcode)
+    if(zipcode.length>=8){
+      cep(zipcode).then(res => {
+        console.log(res)
+        setAddress({
+          state: res.state,
+          city: res.city,
+          neighborhood: res.neighborhood,
+          street: res.street,
+        })
+      })
+    }
+  },[zipcode])
 
   function handleSubmit(data){
     console.log(data)
+    dispach({type: 'setFirstName', value: data.firstName})
+    dispach({type: 'setLastName', value: data.lastName})
+    dispach({type: 'setEmail', value: data.email})
+    dispach({type: 'setZipcode', value: data.zipcode})
+    dispach({type: 'setAddress', value:address})
+    console.log(endereco)
   }
 
   return (
   <Container component='main' maxWidth='xs' >
     <div className={paper}>
-      <Typography variant='h5'>Hello, start your register now!</Typography>
+      <Typography variant='h5'>Hello, start your register now! </Typography>
       <Form className={form} onSubmit={handleSubmit}>
         <Grid container spacing={2}>
           <Grid item xs={12} sm={6}>
@@ -35,7 +64,7 @@ function Signup() {
               name='lastname'
               label='Last Name'
               variant='outlined'
-              required
+              
             />
           </Grid>
           <Grid item xs={12}>
@@ -44,7 +73,7 @@ function Signup() {
                 fullWidth
                 label="Email Address"
                 name="email"
-                required
+                
               />
             </Grid>
           <Grid item xs={12}>
@@ -53,9 +82,11 @@ function Signup() {
             fullWidth
             label='Zipcode'
             variant='outlined'
+            onChange={e => setZipcode(e.target.value)}
             />
           </Grid>
-          <Button
+          {StatusAddress ? (
+            <Button
             type="submit"
             fullWidth
             variant="contained"
@@ -64,6 +95,14 @@ function Signup() {
           >
             Sign Up
           </Button>
+          ):(
+          <Button
+          onClick={()=>{}}
+          fullWidth
+          variant='contained'
+          color='inherit'  
+          >Next</Button>)}
+          
         </Grid>
       </Form>
       <Box mt={5}>
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,27 @@
 import { createStore } from 'redux';
 
 const INITIAL_STATE = {
-  
+  firstName: '',
+  lastName: '',
+  email: '',
+  zipcode: '',
+  address: {},
 }
 
 function change_form(state = INITIAL_STATE, action){
   switch(action.type){
-    
+    case 'setFirstName':
+      return {...state, firstName: action.value};
+    case 'setLastName':
+      return {...state, lastName: action.value};
+    case 'setEmail':
+      return {...state, email: action.value};
+    case 'setZipcode':
+      return {...state, zipcode: action.value}
+    case 'setAddress':
+      return {...state, address: action.value,}
+    default:
+      return state
   }
 }
 
