9ce93a3251e77bab7f9116d4a8d1688ef3864911
Jean Junior <jclgjunior@gmail.com>
2020-05-16 12:39:10 -0300

    Redux schema created

---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import { createStore } from 'redux';
+
+const INITIAL_STATE = {
+  
+}
+
+function change_form(state = INITIAL_STATE, action){
+  switch(action.type){
+
+  }
+}
+
+const store = createStore(change_form);
+
+export default store;
