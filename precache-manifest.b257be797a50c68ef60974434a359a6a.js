1ea3baf0ee404f3eff977d67d323be41a7efe246
Jean Junior <jclgjunior@gmail.com>
2020-05-26 11:56:58 -0300

    Updates

---
+++ precache-manifest.b257be797a50c68ef60974434a359a6a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7de56872f314856edb9dda2437582923",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ab5356fd46ce8fc8871",
+    "url": "/static/js/2.3c9cd2bf.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/static/js/2.3c9cd2bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b5707c15ec2391f3bea",
+    "url": "/static/js/main.87c29f82.chunk.js"
+  },
+  {
+    "revision": "a6ba3f40a9d30a643213",
+    "url": "/static/js/runtime-main.94a12819.js"
+  }
+]);
