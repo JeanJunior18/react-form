118a78a3b5e21e44a7b223c730d15f06d6111506
Jean Junior <jclgjunior@gmail.com>
2020-05-26 12:09:15 -0300

    Updates

---
+++ precache-manifest.bc9570c415495945a2ff37a60c94e907.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39ddad5b8d5fe8efabdf8c8e4a3267f3",
+    "url": "/react-form/index.html"
+  },
+  {
+    "revision": "4ab5356fd46ce8fc8871",
+    "url": "/react-form/static/js/2.3c9cd2bf.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/react-form/static/js/2.3c9cd2bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b5707c15ec2391f3bea",
+    "url": "/react-form/static/js/main.87c29f82.chunk.js"
+  },
+  {
+    "revision": "f3ab5eb91b738c163f4d",
+    "url": "/react-form/static/js/runtime-main.b7d8c408.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b257be797a50c68ef60974434a359a6a.js"
+  "/react-form/precache-manifest.bc9570c415495945a2ff37a60c94e907.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-form/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
